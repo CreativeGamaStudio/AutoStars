6ddc0a05377372b37618104d5ad33512f359fdf7
rasyidf <ufarasfa@gmail.com>
2022-12-26 21:06:07 +0700

    Update Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Item::factory(10)->create();
     }
 }

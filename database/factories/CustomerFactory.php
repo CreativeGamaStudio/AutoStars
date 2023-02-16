3b2a7b244b74256319ffb555d7fff6e1c388dc0f
Nadziraraa <nadziraazahra10@gmail.com>
2023-02-16 13:08:01 +0700

    sync

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'phone_number' => fake()->phone_number(),
+            'address' => fake()->address(),
+            'city' => fake()->city(),
+        ];
+    }
+}

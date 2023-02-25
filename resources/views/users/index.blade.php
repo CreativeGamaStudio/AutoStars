a02eb307138ca76f3fc028e429a9e60fb275c74d
LaelaWulida <laela27811@gmail.com>
2023-02-25 14:01:08 +0700

    user update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -76,14 +76,21 @@
     <x-modal id="modal-edit-user">
         <x-slot:title>Edit</x-slot:title>
         {{ Form::open(array('url' => '/', 'method' => 'PUT', 'class'=>'col-md-12')) }}
-        <input type="hidden" name="_method" value="PUT"/>
             @csrf
-            <<x-input id="name" name="name" label="Name" placeholder="Name" />
+            <input type="hidden" name="_method" value="PUT"/>
+            <x-input id="name" name="name" label="Name" placeholder="Name" />
             <x-input id="email" name="email" label="Email" placeholder="Email" type="email" />
-            <x-input id="password" name="password" label="Password" placeholder="Password" />
+            <!-- <x-input id="password" name="password" label="Password" placeholder="Password" />
             <x-input id="password_confirmation" name="password_confirmation" label="Password Confirmation"
-                placeholder="Password Confirmation" />
-            <x-input id="role" name="role" label="Role" placeholder="Role" />
+                placeholder="Password Confirmation" /> -->
+            <div class="mb-3">
+                <label for="role" class="form-label">Role</label>
+                <select class="form-select" id="role" name="role" placeholder="Pilih Peran">
+                    <option value="admin">Admin</option>
+                    <option value="user">user</option>
+                </select>
+            </div>
+            <!-- <x-input id="role" name="role" label="Role" placeholder="Role" /> -->
             <div class="mb-3">
                 <label for="status" class="form-label">Status</label>
                 <select class="form-select" id="status" name="status">
@@ -96,22 +103,58 @@
         {{ Form::close()}}
     </x-modal>
 
-    <x-modal id="modal-delete-user" size="sm">
-        <x-slot:title>Delete</x-slot:title>
-        <div class="modal-body text-center py-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24" height="24"
-                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M12 9v2m0 4v.01"></path>
-                <path d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75">
-                </path>
-            </svg>
-            <h3>Are you sure?</h3>
-            <div class="text-muted">Apakah anda yakin ingin menghapus data ini?</div>
-            {{-- <input id="dataId" type="text"/> --}}
-        </div>
-        <form action="{{ route('users.destroy', 'id') }}" method="post">
+    {{-- script --}}
+    <script>
+        var exampleModal = document.getElementById('modal-edit-user')
+        exampleModal.addEventListener('show.bs.modal', function(event) {
+            var button = event.relatedTarget
+            var data = button.getAttribute('data-bs-user')
+            // console.log(data);
+            data = data.replace(/'/g, '"');
+            var json = JSON.parse(data);
+
+            var inputName = document.getElementById('modal-edit-user').querySelector('#name');
+            var inputEmail = document.getElementById('modal-edit-user').querySelector('#email');
+            // var inputPass = document.getElementById('modal-edit-user').querySelector('#password');
+            // var inputPassConf = document.getElementById('modal-edit-user').querySelector('#password_confirmation');
+            var inputRole = document.getElementById('modal-edit-user').querySelector('#role');
+            var inputStatus = document.getElementById('modal-edit-user').querySelector('#status');
+
+            inputName.value = json.name;
+            inputEmail.value = json.email;
+            // inputPass.value = json.password;
+            // inputPassConf.value = json.password_confirmation;
+            inputRole.value = json.role;
+            inputStatus.value = json.status;
+
+            var modalTitle = exampleModal.querySelector('.modal-title')
+
+            modalTitle.textContent = 'Edit ' + json.name
+
+            // set action to form
+            var modalForm = document.getElementById('modal-edit-user').querySelector('form');
+            modalForm.action = "/users/" + json.id ;
+        })
+    </script>
+    
+    {{-- Delete --}}
+        {{-- Delete --}}
+        <x-modal id="modal-delete-user" size="sm">
+            <x-slot:title>Delete</x-slot:title>
+            <div class="modal-body text-center py-4">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24" height="24"
+                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M12 9v2m0 4v.01"></path>
+                    <path d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75">
+                    </path>
+                </svg>
+                <h3>Are you sure?</h3>
+                <div class="text-muted">Apakah anda yakin ingin menghapus data ini?</div>
+                {{-- <input id="dataId" type="text"/> --}}
+            </div>
+            <form action="{{ route('users.destroy', 'id') }}" method="post">
             @csrf
             @method('DELETE')
             <input id="id" name="id" hidden>
@@ -148,39 +191,7 @@
         })
     </script>
 
-    {{-- script --}}
-    <script>
-        var exampleModal = document.getElementById('modal-edit-user')
-        exampleModal.addEventListener('show.bs.modal', function(event) {
-            var button = event.relatedTarget
-            var data = button.getAttribute('data-bs-user')
-            console.log(data);
-            data = data.replace(/'/g, '"');
-            var json = JSON.parse(data);
-
-            var inputName = document.getElementById('modal-edit-user').querySelector('#name');
-            var inputEmail = document.getElementById('modal-edit-user').querySelector('#email');
-            // var inputPass = document.getElementById('modal-edit-user').querySelector('#password');
-            // var inputPassConf = document.getElementById('modal-edit-user').querySelector('#password_confirmation');
-            var inputRole = document.getElementById('modal-edit-user').querySelector('#role');
-            var inputStatus = document.getElementById('modal-edit-user').querySelector('#status');
-
-            inputName.value = json.name;
-            inputEmail.value = json.email;
-            // inputPass.value = json.password;
-            // inputPassConf.value = json.password_confirmation;
-            inputRole.value = json.role;
-            inputStatus.value = json.status;
-
-            var modalTitle = exampleModal.querySelector('.modal-title')
-
-            modalTitle.textContent = 'Edit ' + json.name
-
-            // set action to form
-            var modalForm = document.getElementById('modal-edit-user').querySelector('form');
-            modalForm.action = "/users/" + json.id ;
-        })
-    </script>
+    
 @endsection
 
 

f84ae9c0fff75e50c994db1c93676d67a1276861
wachidmuslih <wachidmslh@gmail.com>
2023-04-04 23:19:30 +0700

    update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
 
-                    {{ __('You are logged in!') }}
+
+                        {{-- {{ __('You are logged in!') }} --}}
+
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="row w-50">
+            <div class="col-6">
+
+                <x-conversion-rate-widget title="Customer" :conversion-rate="75" :percentage-change="10" change-direction="up" />
+            </div>
+            <div class="col-6">
+                <x-conversion-rate-widget title="Invoice" :conversion-rate="80" :percentage-change="7" change-direction="down" />
+
+            </div>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,16 +44,7 @@
 
 
             </div>
-            <div class="row w-50">
-                <div class="col-6">
-
-                    <x-conversion-rate-widget title="Customer" :conversion-rate="75" :percentage-change="10" change-direction="up" />
-                </div>
-                <div class="col-6">
-                    <x-conversion-rate-widget title="Invoice" :conversion-rate="80" :percentage-change="7" change-direction="down" />
-
-                </div>
-            </div>
+            
         </div>
         <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta16/dist/js/tabler.min.js"></script>
     </body>
